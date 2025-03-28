628249120bee18521d0617ce9698bd9b6d80ec47
reddyere <137512186+reddyere@users.noreply.github.com>
2025-03-28 19:48:44 +0530

    Add streaming-platform/client/src/types/react-icons.d.ts 1743171529563

---
+++ streaming-platform/client/src/types/react-icons.d.ts
@@ -0,0 +1,8 @@
+declare module 'react-icons/fa' {
+  import { FC } from 'react';
+  export const FaPlay: FC<{size?: number}>;
+  export const FaPause: FC<{size?: number}>;
+  export const FaVolumeUp: FC<{size?: number}>;
+  export const FaVolumeMute: FC<{size?: number}>;
+  export const FaCog: FC<{size?: number}>;
+}
