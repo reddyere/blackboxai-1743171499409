d6815f4957f5396dc0fa110f3eb828e8a31a2098
reddyere <137512186+reddyere@users.noreply.github.com>
2025-03-28 19:48:35 +0530

    Add streaming-platform/client/tailwind.config.js 1743171520668

---
+++ streaming-platform/client/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#9147ff',
+        secondary: '#772ce8',
+        dark: '#0e0e10',
+        light: '#efeff1'
+      },
+    },
+  },
+  plugins: [],
+}
