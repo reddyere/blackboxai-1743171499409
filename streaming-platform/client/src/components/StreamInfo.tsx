a419576d159188f092529cb4613807e4aa643b5a
reddyere <137512186+reddyere@users.noreply.github.com>
2025-03-28 19:48:49 +0530

    Add streaming-platform/client/src/components/StreamInfo.tsx 1743171535498

---
+++ streaming-platform/client/src/components/StreamInfo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const StreamInfo = () => {
+  return (
+    <div className="stream-info">
+      {/* Stream information will be displayed here */}
+      <h2>Stream Information</h2>
+    </div>
+  );
+};
+
+export default StreamInfo;
