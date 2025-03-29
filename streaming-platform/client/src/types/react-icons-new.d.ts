49cd3ff5fb5c6ce124114ef0b244ca86db877e9a
reddyere <137512186+reddyere@users.noreply.github.com>
2025-03-29 11:56:46 +0530

    Add streaming-platform/client/src/types/react-icons-new.d.ts 1743229610947

---
+++ streaming-platform/client/src/types/react-icons-new.d.ts
@@ -0,0 +1,6 @@
+declare module 'react-icons/fa' {
+  import { IconType } from 'react-icons/lib/esm/iconBase';
+  export const FaTrash: IconType;
+  export const FaPaperPlane: IconType;
+  export const FaBan: IconType;
+}
